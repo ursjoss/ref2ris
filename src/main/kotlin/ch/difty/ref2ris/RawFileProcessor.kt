57ab68e21c6406ddf8edcaf90c10b8adb7c0b4dc
Urs Joss <urs.joss@gmx.ch>
2025-02-22 18:21:42 +0100

    Write to output.ris

--- src/main/kotlin/ch/difty/ref2ris/RawFileProcessor.kt
+++ src/main/kotlin/ch/difty/ref2ris/RawFileProcessor.kt
@@ -1,12 +1,19 @@
 package ch.difty.ref2ris
 
 import ch.difty.kris.domain.RisRecord
+import ch.difty.kris.toRisLines
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.io.BufferedWriter
+import java.nio.charset.Charset
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 
 /**
  * [RawFileProcessor] interface configuring processing pipelines for the different file imports.
@@ -27,16 +34,19 @@ internal enum class RawFiles : RawFileProcessor {
             toTextLines()
                 .toRawReference()
                 .toRisRecord { it.toRisRecord() }
-                .printToConsole()
+                .toRisLines()
+                .writeCleanFileLineTo(outputPath)
+
     },
 }
 
+@Suppress("unused")
 @ExperimentalCoroutinesApi
-internal fun Flow<RisRecord>.printToConsole(): Flow<Int> = flow {
+internal fun Flow<String>.printToConsole(): Flow<Int> = flow {
     var lineCount = 0
     println()
     collect { risLine ->
-        println(risLine.toString())
+        print(risLine.toString())
         lineCount++
     }
     emit(lineCount)
@@ -64,25 +74,23 @@ internal fun Flow<RawReference>.toRisRecord(f: (RawReference) -> RisRecord): Flo
     collect { rawReference -> emit(f(rawReference)) }
 }
 
-//@ExperimentalCoroutinesApi
-//private fun <T : CleanFileLine> Flow<T>.writeCleanFileLineToJson(path: Path, outputPath: String): Flow<Int> =
-//    flow {
-//        var lines = 0
-////        openCleanWriter(monthRoot, outputPath).use { writer ->
-////            collect { line ->
-////                val json = Gson().toJson(line)
-////                writer.appendLine(json)
-////                lines++
-////            }
-////        }
-//        emit(lines)
-//    }.flowOn(Dispatchers.IO)
+@ExperimentalCoroutinesApi
+private fun Flow<String>.writeCleanFileLineTo(outputPath: String): Flow<Int> =
+    flow {
+        var lines = 0
+        openCleanWriter(outputPath).use { writer ->
+            collect { line ->
+                writer.append(line)
+                lines++
+            }
+        }
+        emit(lines)
+    }.flowOn(Dispatchers.IO)
 
-//private fun openCleanWriter(monthRoot: Path, targetFileName: String): BufferedWriter {
-//    fun Path.openBufferedWriter(charset: Charset = Charsets.UTF_8): BufferedWriter =
-//        Files.newBufferedWriter(this, charset, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
-//
-//    val file = monthRoot.resolve(targetFileName)
-//    Files.createDirectories(file.parent)
-//    return file.openBufferedWriter()
-//}
+private fun openCleanWriter(targetFileName: String): BufferedWriter {
+    fun Path.openBufferedWriter(charset: Charset = Charsets.UTF_8): BufferedWriter =
+        Files.newBufferedWriter(this, charset, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
+
+    val file = Path.of(targetFileName)
+    return file.openBufferedWriter()
+}
